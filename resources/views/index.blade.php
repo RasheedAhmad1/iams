77f2a24fc69363171f5bd382fd31e226f60152cc
Ammar-gith <aammarulhasan094@gmail.com>
2024-12-05 14:17:04 +0500

    feat(view): add index and their route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.masterVertical')
+
+
+@push('style')
+    <style>
+        /* custom css goes here */
+        .head {
+            color: yellow;
+        }
+    </style>
+@endpush
+
+@push('content')
+    <h1 class="head">Rasheed Khan</h1>
+@endpush
